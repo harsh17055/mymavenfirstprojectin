7ed3fbb46c8fc79bba43dc966458b12fe73fdb93
harsh17055 <harshangral1234567890@gmail.com>
2026-02-02 22:36:45 +0530

    new change

---
+++ src/main/java/com/encapsulation/Mainmultiply.java
@@ -0,0 +1,11 @@
+package com.encapsulation;
+
+public class Mainmultiply {
+    public static void main(String[] args) {
+        Multiply multiply = new Multiply();
+        multiply.multiply(8,9);
+
+
+    }
+
+}
