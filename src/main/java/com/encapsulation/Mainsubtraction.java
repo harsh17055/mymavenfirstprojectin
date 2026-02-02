aad6ace05ff9bae646326a1af5cd3b30fb0c97ec
harsh17055 <harshangral1234567890@gmail.com>
2026-02-02 21:50:50 +0530

    new project

---
+++ src/main/java/com/encapsulation/Mainsubtraction.java
@@ -0,0 +1,8 @@
+package com.encapsulation;
+
+public class Mainsubtraction {
+    public static void main(String[] args) {
+        Subtraction subtraction = new Subtraction();
+        subtraction.subtraction(8,9);
+    }
+}
